e77376a9862e660881251cee2240bff46881e174
Eric Boutueil <e.boutueil@gmail.com>
2018-02-12 16:36:10 +0100

    new game done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,10 @@
+class GamesController < ApplicationController
+  def new
+    # @letters = [*('A'..'C')].sample(10)
+    @letters = (0...10).map { ('A'..'Z').to_a[rand(26)] }
+    # raise
+  end
+
+  def score
+  end
+end
