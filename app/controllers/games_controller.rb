9433c9ad249fc2b162383ad5a559d44c2272b771
Eric Boutueil <e.boutueil@gmail.com>
2018-02-12 17:41:04 +0100

    computing the score WIP

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,7 +6,15 @@ class GamesController < ApplicationController
   end
 
   def score
-    @play = params[:play]
+    @word = params[:word]
+    word_array = @word.split("")
+    letters_array = params[:letters].split("")
+    word_array.each do |letter|
+      if !letters_array.include?("letter")
+          @result =
     # binding.pry
+    # The word can't be built out of the original grid
+    # The word is valid according to the grid, but is not a valid English word
+    # The word is valid according to the grid and is an English word
   end
 end
